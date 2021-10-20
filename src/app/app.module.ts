7f9cfe92a398eaab258b61ba58e3fe6d90b6d314
AbdielP <abdiel.pinzonc@gmail.com>
2021-10-19 19:01:52 -0500

    nav and map component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
