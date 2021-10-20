bf8b26a88917a55d4723d9e0ca6c37369c1b7733
AbdielP <abdiel.pinzonc@gmail.com>
2021-10-19 20:29:37 -0500

    Leaflet map integrated with mapbox tile maps and ip address location with openstreetmap

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  private KEY = environment.IPifyToken;
+  map: L;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.createMap();
+  }
+
+  private createMap(): void {
+    this.map = L.map('map').setView([8.95255, -79.53548], 13); // Lat, long, zoom
+    L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${this.KEY}`, {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: `${this.KEY}`
+    }).addTo(this.map);
+
+    L.marker([8.95255, -79.53548]).addTo(this.map)
+    .bindPopup('Lat: 8.95255, Lng: -79.53548')
+    .openPopup();
   }
 
 }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private KEY = environment.IPifyToken;
+
+  constructor(private http: HttpClient) { }
+
+}
